2fbea33f45fbc5bce977c5463a0178658931165e
sawakowakasugi-web <sawako.wakasugi@qenest.com>
2026-08-31 14:43:24 +0900

    practice-day-03

---
+++ app/practice/day-03/page.tsx
@@ -0,0 +1,9 @@
+export default function Day03Page() {
+  return (
+    <ul>
+        <li>JavascriptとTypescriptはプログラミング言語</li>
+        <li>Typescriptはエラーが帰ってくる</li>
+        <li>Next.jsのAppRouterという機能で名前がそのままURLに</li>
+    </ul>
+  );
+}

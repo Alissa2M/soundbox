54ed06d1dfb60d0270c1da1d5977e2f8f6617461
sawakowakasugi-web <sawako.wakasugi@qenest.com>
2026-09-04 15:57:10 +0900

    practice-day-04

---
+++ app/practice/day-04/page.tsx
@@ -0,0 +1,11 @@
+export default function Day04Page() {
+  return (
+    <ul>
+        <li>CompleteLessonButton.module.css</li>
+        <li>CompleteLessonButton.tsx</li>
+        <li>Sticky.module.css</li>
+        <li>Sticky.tsx</li>
+        <li>progress.ts</li>
+    </ul>
+  );
+}
